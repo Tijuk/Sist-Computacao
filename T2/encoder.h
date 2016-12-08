1e73635db6c94b2540d373b1a3f87bf0106a78cd
Tijuk <tijuk00@hotmail.com>
2016-12-08 19:40:47 -0200

    Create encoder.h

---
+++ T2/encoder.h
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/***********************\
+*	FUNCOES DO CLIENTE	*
+\***********************/
+void decode(char *code);
+
+
+/***********************\
+*	FUNCOES DO SERVER	*
+\***********************/
+
+void start_code(void);
+/*
+	start_code = inicia o encode(inicia as variaveis)
+	deve ser chamada toda vez que quiser iniciar um NOVO encode
+*/
+void ins_path(char *path);
+/*
+	ins_path, joga o path(JA CORTADO) dentro de uma pilha(vetor)
+*/
+
+char* encode(void);
+/*
+	retorna a string codificada
+*/
