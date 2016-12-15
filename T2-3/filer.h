3f2968273b8b2efab4b7296ecb36408f05ac07e6
Tijuk <tijuk00@hotmail.com>
2016-12-15 09:14:18 -0200

    Update filer.h

--- T2-3/filer.h
+++ T2-3/filer.h
@@ -8,18 +8,9 @@
 #include <sys/mman.h> 
 #include <stdlib.h>
 #include <string.h>
-#include <stdio.h>
-#include <stdlib.h>
 #include <dirent.h>
 #include <sys/param.h>
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include <errno.h>
-#include <unistd.h>
-#include <fcntl.h>
-#include <sys/stat.h>
-#include <sys/types.h>
 #include <sys/xattr.h>
 
 
